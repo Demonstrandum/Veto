ebb9701f8ce6010da37e0b88b8a26b0a796c28b4
Demonstrandum <knutsen@jetspace.co>
2018-11-20 19:08:36 +0000

    Seperate code to appropriate files.

--- public/new.js
+++ public/new.js
@@ -66,3 +66,146 @@ $('document').ready(() => {
     return false;
   });
 });
+
+
+let editing = NaN;
+
+const add_list = option => {
+  $('#addition').val('');
+  let adder = $(".option").detach();
+  $("#options")
+    .append(`
+      <li class="option-item">
+        <span class="value">${option}</span>
+        <div class="symbols">
+          <i class="option-edit far fa-edit"></i>
+          <i class="option-remove fas fa-times"></i>
+        </div>
+      </li>
+    `)
+    .append(adder);
+  $('#addition').focus().select();
+};
+
+
+$('#options').on('click', '.option-remove', e => {
+  const li = $(e.target).closest('li');
+  li.slideUp(100, function() { $(this).remove(); });
+  console.warn(li[0], 'Element been removed!');
+});
+
+const save = () => {
+  const inputs = $('#options li').children('input');
+  let values = Array(...$('#options li span')
+                .filter((i, e) => e.style.display !== 'none')
+                .map((i, e) => e.innerHTML));
+  console.log(values);
+
+  let bad_input = null;
+  inputs.each(function() {
+    const value = this.value;
+    if (value.length === 0) {
+      issue(ISSUE.FATAL, `Cannot leave new value empty. Delete it if you don't want it.`);
+      bad_input = $(this);
+      return;
+    }
+    if (values.includes(value)) {
+      issue(ISSUE.FATAL, `Cannot have duplicate (primary) options/choices!`);
+      bad_input = $(this);
+      return;
+    }
+
+    $(this).parent('li').removeClass('dark-option');
+    $(this).siblings('span').show().text(value);
+    $(this).siblings('.symbols').find('.option-edit')
+      .removeClass('fa-save')
+      .addClass('fa-edit');
+    this.remove();
+  });
+
+  if (bad_input !== null) {
+    bad_input.attention();
+    return false;
+  }
+
+  let value = $("#addition").val().trim();
+
+  if (values.includes(value)) {
+    issue(ISSUE.FATAL, `Cannot have duplicate (primary) options/choices!`);
+    $('#addition').attention();
+    return false;
+  }
+
+
+  if (value.length > 0) add_list(value);
+  return true;
+};
+
+$(document).on('click', e => {
+  if (!( $(e.target).hasClass('editor')
+      || $(e.target).hasClass('option-item')
+      || $(e.target).hasClass('fa-save')
+      || $(e.target).hasClass('value')
+    )) {
+    save();
+    editing = NaN;
+  }
+});
+
+const edit_mode = li => {
+  const span = li.find('span');
+  span.hide();
+  li.addClass('dark-option');
+  li.find('.option-edit')
+    .removeClass('fa-edit')
+    .addClass('fa-save');
+  li.prepend(`<input class="editor" type="text" value="${span.text()}" />`);
+  li.find('input').focus().select();
+};
+
+$('#options').on('click', 'li', e => {
+  if (e.target.nodeName === 'INPUT') return;
+  let li = $(e.currentTarget);
+  save();
+
+  if (editing !== li.index()) {
+    editing = li.index();
+    edit_mode(li);
+  } else {
+    editing = NaN;
+  }
+});
+
+$(document).on('keypress', '.editor', key => {
+  if (key.which === 13 || key.which === 10) {
+    key.preventDefault();
+    save();
+    editing = NaN;
+  }
+});
+
+$("#addition").on('keypress', key => {
+  let value = $("#addition").val().trim();
+  if (key.which === 13 || key.which === 10) {
+    if (value.length === 0) {
+      return $('#addition').attention();
+    }
+    key.preventDefault();
+    save();
+  }
+});
+$('.add-option').click(() => {
+  if ($('#addition').val().length === 0) {
+    $('#addition').attention();
+    return;
+  }
+  save();
+});
+
+$("#title").on('input', () => {
+  $("#code").val(encodeURIComponent($("#title").val().toLowerCase().replace(/\s|\\|\//g, '-')));
+});
+
+$('#code')[0].addEventListener('keyup', e => {
+  $("#code").val(encodeURIComponent($("#code").val().replace(/[^A-Za-z\d\-\.\!\']/g, '-')));
+});
---
+++ public/stats.js
@@ -0,0 +1,97 @@
+Chart.defaults.global.elements.arc.borderColor = '#ddd';
+const ALPHA_RANGE = {
+  min: 0.08,
+  max: 1
+}
+
+const chart_colors = chart => {
+  const range = ALPHA_RANGE;
+  const N = chart.data.datasets[0].data.length;
+  backgrounds = Array(N);
+  for (let mono = range.min; mono <= range.max; mono += range.max / (N + 1))
+    backgrounds[Math.round((mono - range.min) * N / range.max)] = `rgba(0,0,0,${mono})`;
+  chart.data.datasets[0].backgroundColor = backgrounds;
+  return backgrounds;
+};
+
+const chart_empty = chart => {
+  chart.data.labels = [];
+  chart.data.datasets[0].data = [];
+};
+
+const push_data = (chart, label, data) => {
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data.push(data);
+  });
+}
+
+$(document).ready(() => {
+  $('.home').click(() => {
+    window.location.href = '/';
+  })
+  for (const canvas of ['#pie', '#bar']) {
+    const context = $(canvas)[0];
+    const dimension = $(canvas).closest('div').innerWidth() - 22;
+    context.height = dimension;
+    context.width = dimension;
+  }
+});
+
+
+// CHARTS:
+let pie;
+let bar;  // Make sure they're global.
+$(document).ready(() => {
+  const pie_context = $('#pie');
+
+  pie = new Chart(pie_context[0], {
+    type: 'pie',
+    data: {
+      labels: [],
+      datasets: [{
+        label: '№	of Votes',
+        data: [],
+        backgroundColor: [],
+      }]
+    },
+    options: {}
+  });
+
+  const bar_context = $('#bar')
+  bar = new Chart(bar_context[0], {
+    type: 'horizontalBar',
+    data: {
+      labels: [],
+      datasets: [{
+        label: '№	of Votes',
+        data: [],
+        backgroundColor: [],
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Distribution of alternative/other votes.'
+      },
+      scales: {
+        yAxes: [{
+          categoryPercentage: 0.9,
+          barPercentage: 1.0,
+          ticks: {
+            mirror: true,
+            padding: -10,
+          }
+        }],
+        xAxes: [{
+          ticks: {
+            stepSize: 1
+          }
+        }]
+      },
+      legend: {
+        display: false,
+      }
+    }
+  });
+});
